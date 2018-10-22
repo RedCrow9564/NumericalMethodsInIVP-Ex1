75476065300c6e3db712c5df7ed5fd6fcc55cec2
אלעד איטח <elad9564@gmail.com>
2018-10-23 00:25:57 +0300

    Made runtime improvement by switching to direct calculation of the convolution, while ignoring zero terms. Began implementing the Leap-Frog model for the advection equation.

--- Infrastructure/numeric_schemes.py
+++ Infrastructure/numeric_schemes.py
@@ -1,9 +1,9 @@
 import numpy as np
-from numpy.fft import fft, ifft
+
 
 class _ModelTemplate(object):
     def __init__(self, n, dt, last_t, first_x, last_x, starting_condition_func, nonhomogeneous_term):
-        self.x_samples = np.linspace(first_x, last_x, n)
+        self.x_samples = np.linspace(first_x, last_x, n + 1)
         self.t_samples = np.arange(0, last_t, dt)
         self._dx = self.x_samples[1] - self.x_samples[0]
         self._dt = dt
@@ -11,7 +11,7 @@ class _ModelTemplate(object):
         self._current_state = starting_condition_func(self.x_samples)
         self._nonhomogeneous_term = nonhomogeneous_term
 
-    def make_steps(self, steps_num):
+    def make_step(self):
         raise NotImplementedError("Each model MUST implement this method!")
 
 
@@ -21,18 +21,35 @@ class AdvectionEqForwardEuler(_ModelTemplate):
         super(AdvectionEqForwardEuler, self).__init__(n, dt, last_t, first_x, last_x, starting_condition_func,
                                                       nonhomogeneous_term)
 
-        ratio = self._dt / (2 * self._dx)
-        unity_roots = roots_of_unity(n)[1:]
-        self._transition_mat_eigenvalues = 1 + ratio * (unity_roots - np.flip(unity_roots, axis=0))
+        self._ratio = self._dt / (2 * self._dx)
 
-    def make_steps(self, steps_num=1):
-        eigenvalues_power = np.power(self._transition_mat_eigenvalues, np.mod(steps_num, self._n))
-        self._current_state = fft(np.multiply(eigenvalues_power, ifft(self._current_state)))
+    def make_step(self):
+        res = []
+        i1 = 0
+        i2 = 1
+        i3 = self._n
+        k = 0
+        for _ in range(self._n + 1):
+            resk = self._current_state[i1] + self._ratio * self._current_state[i2] - self._ratio * self._current_state[i3]
+            i1 += 1
+            i2 += 1
+            i3 += 1
+            if k == 0:
+                i3 = 0
+            elif k == self._n - 1:
+                i2 = 0
+            k += 1
+            res.append(resk)
+        self._current_state = np.array(res)
         return self._current_state
 
 
-def roots_of_unity(n):
-    primitive_root = np.exp(2 * np.pi * 1j / n)
-    roots = np.power(primitive_root, list(range(1, n)))
-    roots = np.append([1], roots)
-    return roots
+class AdvectionModelLeapFrog(_ModelTemplate):
+    def __init__(self, n, dt, last_t, first_x, last_x, starting_condition_func):
+        nonhomogeneous_term = 0  # The equation is assumed to be homogeneous.
+        super(AdvectionModelLeapFrog, self).__init__(n, dt, last_t, first_x, last_x, starting_condition_func,
+                                                      nonhomogeneous_term)
+        self._forward_euler_first_step_model = AdvectionEqForwardEuler(n, dt, last_t, first_x, last_x,
+                                                                       starting_condition_func)
+
+        self._ratio = self._dt / (2 * self._dx)
--- main.py
+++ main.py
@@ -1,43 +1,97 @@
 import timeit
 import numpy as np
 from scipy import sparse
+from Infrastructure.numeric_schemes import AdvectionEqForwardEuler
+from copy import deepcopy
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+
+
+def direct_powers(n):
+  a = np.exp(2 * np.pi * 1j / n)
+  p = np.power(a, list(range(1, n)))
+  p = [1] + p.tolist()
+  return p
+
+
+def direct_prod(n, v):
+    a = 2
+    diagonals = [np.ones(n), a * np.ones(n - 1), -a * np.ones(n - 1), [-a], [a]]
+    A = sparse.diags(diagonals, offsets=[0, 1, -1, n - 1, -n + 1]).toarray()
+    result = A.dot(v)
+    return result, A
+
+
+def deco_product(n, v):
+    a = 2
+    unity_roots = np.array(direct_powers(n)[1:])
+    eigen = a * (unity_roots - np.flip(unity_roots, axis=0)) + 1
+    eigen = np.conj(eigen)
+    eigen = np.append([1], eigen)
+    result = np.fft.fft(np.multiply(eigen, np.fft.ifft(v)))
+    return result, eigen
+
+
+def conv_product(n, v):
+    a = 2
+    v_list = v.tolist()
+    res = []
+    i1 = 0
+    i2 = 1
+    i3 = n - 1
+    k = 0
+    for vk in v_list:
+        resk = 1 * v[i1] + a * v[i2] - a * v[i3]
+        i1 += 1
+        i2 += 1
+        i3 += 1
+        if k == 0:
+            i3 = 0
+        elif k == n - 2:
+            i2 = 0
+        k += 1
+        res.append(resk)
+    res = np.array(res)
+    return res
 
 
 def main():
-    gamma = 1
-    N = 5
-    last_t = 5
-
+    start_condition = lambda x: np.cos(2 * np.pi * x)
+    gamma = 0.1
+    N = 16
     dx = 1 / (N + 1)
     dt = gamma * dx
+    last_t = 5
+    x = np.arange(0, 1, dx)
+    t = np.arange(0, last_t, dt)
+    X, T = np.meshgrid(x, t)
+    current_state = start_condition(x.reshape((1, -1)))
+    numeric_solution = deepcopy(current_state)
 
-    a = dt/(2 * dx)
-    main_diag = np.ones(N + 1)
-    upper_main_diag = a * np.ones(N + 1)
-    under_main_diag = -a * np.ones(N + 1)
-    top_element = [-a]
-    bottom_element = [a]
-    diagonals = [main_diag, upper_main_diag, under_main_diag, top_element, bottom_element]
-    offsets = np.array([0, 1, -1, N, -N])
-    b = sparse.diags(diagonals, offsets, shape=(N + 1, N + 1), format='dia').tocsr()
+    model = AdvectionEqForwardEuler(N, dt, last_t, first_x=0, last_x=1, starting_condition_func=start_condition)
+    for _ in t.tolist()[1:]:
+        current_state = model.make_step().reshape((1, -1))
+        numeric_solution = np.append(numeric_solution, current_state, axis=0)
 
-    v = np.ones(N + 1)
-    return b, v
+    fig = plt.figure()
+    ax = Axes3D(fig)
+    ax.plot_surface(X, T, numeric_solution, rstride=1, cstride=1)
+    plt.show()
+    print('t')
 
 
-def sparse_product(b, v):
-    b ** 2
+def test():
+    n = 2 ** 4
+    v = np.random.rand(n)
+    glob = {'v': v, 'n': n}
+    repeats = 1000
+    t1 = timeit.timeit("deco_product(n, v)", setup="from __main__ import deco_product", globals=glob, number=repeats) / repeats
+    t2 = timeit.timeit("direct_prod(n, v)", setup="from __main__ import direct_prod", globals=glob, number=repeats) / repeats
+    t3 = timeit.timeit("conv_product(n, v)", setup="from __main__ import conv_product", globals=glob, number=repeats) / repeats
+    print(t1 * 10 ** 6)
+    print(t2 * 10 ** 6)
+    print(t3 * 10 ** 6)
 
 
 if __name__ == '__main__':
-    b, v = main()
-    print(b.toarray())
-    iterations = 2000
-    times = timeit.repeat('sparse_product(b, v)', 'from __main__ import main, sparse_product', repeat=3,
-                          number=iterations, globals={'b': b, 'v': v})
-    print('Program took {0} usec'.format(min(times) * 1000000 / iterations))
-
-    b = b.toarray()
-    times = timeit.repeat('sparse_product(b, v)', 'from __main__ import main, sparse_product', repeat=3,
-                          number=iterations, globals={'b': b, 'v': v})
-    print('Program took {0} usec'.format(min(times) * 1000000 / iterations))
+    main()
